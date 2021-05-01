b303510738ac1da3c58b1ca8a86f14b8d700cf87
Halie Chalkley <magpieshrike@gmail.com>
2021-05-01 16:07:56 -0400

    frame

--- client/src/Components/Frame/index.js
+++ client/src/Components/Frame/index.js
@@ -2,16 +2,31 @@ import React from 'react';
 import { useState } from 'react';
 import Collection from '../Collection';
 import Identify from '../Identify';
+import fCSS from './frame.module.css';
 
 function Frame() {
     const [screen, setScreen] = useState( "search" );
 
         return(
             <div>
-                <button onClick={() => setScreen("search")}>Search</button>
-                <button onClick={() => setScreen("collection")}>Collection</button>
+                    <div className={fCSS.ftop}></div>
+                    <div className={fCSS.ftitle}></div>
+                    <div className={fCSS.fleft}></div>
+                    <div className={fCSS.search}>
+                        <button name="search" className={fCSS.button} onClick={() => setScreen("search")}>SEARCH</button>
+                    </div>
+                    <div className={fCSS.collection}>
+                        <button name="collection" className={fCSS.button} onClick={() => setScreen("collection")}>POKEMON</button>
+                    </div>
+                    <div className={fCSS.fright}></div>
+                    <div className={fCSS.cbottom}></div>
+                    <div className={fCSS.fbottom}></div>
                 
-                {screen == "search" ? <Collection /> : <Identify />}
+                
+                <div className={fCSS.screen}>
+                {screen == "search" ? <Identify /> : <Collection />}
+                </div>
+   
             </div>
         )
 }
